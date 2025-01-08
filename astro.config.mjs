3840ac72217e21dc06f9e7a797e6251afc5b90d2
AgungBahtiarr <agungbahtiar050@gmail.com>
2025-01-08 21:44:28 +0700

    Add Node adapter for standalone deployment in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
+  // adapter: ,
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
